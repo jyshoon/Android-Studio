da770ff4e4f6e8aa645e284be9caa34ff41086de
kimTaeHoon <xogns8121@gmail.com>
2019-04-28 18:00:50 +0900

    #26 issue 4,5,6번 힌트창 비활성화

--- app/src/main/java/com/example/so/project/ReadyRoom.java
+++ app/src/main/java/com/example/so/project/ReadyRoom.java
@@ -36,7 +36,7 @@ public class ReadyRoom extends AppCompatActivity {
     private boolean isConnected = false;
 
     private String addr = "192.168.0.15".trim();
-    private int port = 8022;
+    private int port = 8029;
     private ConnectThread connectThread;
     private ReadyRoomMesgRecv recvThread;
     private MessageHandler mesgHandler;

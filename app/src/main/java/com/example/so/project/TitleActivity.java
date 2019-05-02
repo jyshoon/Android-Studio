ce5801391174b78c1e3629cffea5fff60257df65
kimTaeHoon <xogns8121@gmail.com>
2019-05-03 00:11:36 +0900

    Merge remote-tracking branch 'remotes/origin/태훈1'
    # Conflicts:
    #	.gradle/4.6/fileHashes/fileHashes.bin
    #	.gradle/4.6/fileHashes/fileHashes.lock
    #	.gradle/4.6/taskHistory/taskHistory.bin
    #	.gradle/4.6/taskHistory/taskHistory.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.idea/caches/build_file_checksums.ser
    #	.idea/caches/gradle_models.ser
    #	app/app.iml
    #	app/src/main/java/com/example/so/project/ReadyRoom.java
    #	local.properties

--- app/src/main/java/com/example/so/project/TitleActivity.java
+++ app/src/main/java/com/example/so/project/TitleActivity.java
@@ -3,7 +3,12 @@ package com.example.so.project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.so.project.LoginActivity;
 import com.example.so.project.R;
@@ -21,6 +26,17 @@ public class TitleActivity extends AppCompatActivity {
     public void onButtonLoginClicked(View v){
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivityForResult(intent, REQUEST_CODE_LOGIN);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_layout_root));
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText("Hello! This is a custom toast!");
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
     }
 /////////////
     public void onButtonRegisterClicked(View v){
